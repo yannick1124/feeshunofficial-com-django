55036e6f5033eee98b0fd8af900005e383c27cc7
yannick1124 <yannick.mackor@gmail.com>
2026-05-18 22:20:36 +0200

    updated database default

--- website/settings/base.py
+++ website/settings/base.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': env('DB_ENGINE', default='django.db.backends.sqlite3'),
-        'NAME': env('DB_NAME', default=BASE_DIR / 'db.sqlite3'),
+        'NAME': env('DB_NAME', default=str(BASE_DIR / 'db.sqlite3')),
         'USER': env('DB_USER', default=''),
         'PASSWORD': env('DB_PASSWORD', default=''),
         'HOST': env('DB_HOST', default=''),
